46764cd26d246b7b3849fd096858e7a38ecaf48d
ashikee.abhi <ovi4010@gmail.com>
2015-06-15 22:00:13 +0600

    some basic changes made

--- functions.php
+++ functions.php
@@ -1,26 +1,5 @@
 <?php
 
-function getPrice($find)
-{
-	$books=array(
-		"java" =>320,
-		"c" => 420,
-		"php" => 520
-
-	);
-	
-	foreach( $books as $book=>$price)
-	{
-		//echo $book;
-		//echo "</br>";
-		if($book == $find)
-		{
-			return $price;
-			break;
-		}
-	}
-	/***/
-}
 function deliverResponse($status, $statusMessage, $data)
 {
 	header("HTTP/1.1 $status #statusMessage");
